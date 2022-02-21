a87730b7bb9cd7279039b7de4f50affd2bbc13e1
martipurull <91315241+martipurull@users.noreply.github.com>
2022-02-21 18:53:23 +0100

    adds rest of crud for users

--- src/services/users/index.ts
+++ src/services/users/index.ts
@@ -2,6 +2,7 @@ import e from 'express'
 import express, { NextFunction, Request, Response } from 'express'
 import createHttpError from 'http-errors'
 import { provideTokens, verifyJWTsAndRegenerate } from '../../auth/functions'
+import { JWTAuth } from '../../Middlewares/JWTAuth'
 import { cloudinary, parser } from '../../utils/cloudinary'
 import UserModel from './schema'
 
@@ -42,6 +43,68 @@ usersRouter.post('/refreshToken', async (req: Request, res: Response, next: Next
         const { currentRefreshJWT } = req.body
         const { accessJWT, refreshJWT } = await verifyJWTsAndRegenerate(currentRefreshJWT)
         res.send({ accessJWT, refreshJWT })
+    } catch (error) {
+        next(error)
+    }
+})
+
+usersRouter.get('/', JWTAuth, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const users = await UserModel.find()
+        res.send(users)
+    } catch (error) {
+        next(error)
+    }
+})
+
+usersRouter.get('/me', JWTAuth, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        if (req.payload) {
+            const user = await UserModel.findById(req.payload._id)
+            user ? res.send(user) : next(createHttpError(404, `User with id ${req.payload._id} does not exist.`))
+        } else {
+            next(createHttpError(400, 'Invalid request.'))
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+
+usersRouter.put('/me', JWTAuth, parser.single('userAvatar'), async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        if (req.payload) {
+            const oldUser = await UserModel.findById(req.payload._id)
+            if (oldUser) {
+                const body = { ...req.body, avatar: req.file?.path || oldUser.avatar, filename: req.file?.filename || oldUser.filename }
+                const editedUser = await UserModel.findByIdAndUpdate(req.payload._id, body, { new: true })
+                if (!editedUser) return next(createHttpError(404, `User with id ${req.payload._id} does not exist.`))
+                if (oldUser && req.file) {
+                    await cloudinary.uploader.destroy(oldUser.filename)
+                }
+                res.send(editedUser)
+            } else {
+                next(createHttpError(404, `User with id ${req.payload._id} does not exist.`))
+            }
+        } else {
+            next(createHttpError(400, 'Invalid request.'))
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+
+usersRouter.delete('/me', JWTAuth, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        if (req.payload) {
+            const deletedUser = await UserModel.findByIdAndDelete(req.payload._id)
+            if (!deletedUser) return next(createHttpError(404, `User with id ${req.payload._id} does not exist or has already been deleted.`))
+            if (deletedUser.filename) {
+                await cloudinary.uploader.destroy(deletedUser.filename)
+            }
+            res.status(204).send()
+        } else {
+            next(createHttpError(400, 'Invalid request.'))
+        }
     } catch (error) {
         next(error)
     }
