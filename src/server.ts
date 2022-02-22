10d7d4ee0c5cb76ace9c6c325f67b308e5686e78
martipurull <91315241+martipurull@users.noreply.github.com>
2022-02-22 13:15:14 +0100

    fixes typos

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,16 @@
 import express from "express"
 import cors from 'cors'
 import cookieParser from "cookie-parser"
-import usersRouter from "./Services/Users"
+import usersRouter from "./services/users"
 import { errorHandlers } from "./Middlewares/errorhandlers"
-import contactRouter from "./Services/Contacts"
-import conversationRouter from "./Services/Conversation"
+import contactRouter from "./services/Contacts"
+import conversationRouter from "./services/conversation"
 
 const server = express()
 server.use(cors({ origin: 'http//:localhost:3000', credentials: true }))
 server.use(express.json())
 server.use(cookieParser())
 
-// server.use('/users', usersRouter)
 server.use('/contacts', contactRouter)
 server.use('/conversations', conversationRouter)
 server.use('/users', usersRouter)
